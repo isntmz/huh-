8fae08c1166f85e0ed74c5e1935b4f467dba2fd1
drew <46280563+isntmz@users.noreply.github.com>
2024-04-04 13:32:41 -1000

    Collision Fix
    I FINALLY DID IT LETSFUCKINGOOOOOOOOOOOOOOOOOO WOOOOHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo
    - Fixed player and apple collision, which I literally had to change "this.x" and "this.y" in my collision function to "player.x" and "player.y"
    bruh.
    
    Upcoming:
    - Tutorial instructions update
    - LORE!!! (Story Menu!!!)

--- videogametutorial.js
+++ videogametutorial.js
@@ -7,6 +7,9 @@ let h = window.innerHeight
 let bx = w / 2
 let by = h / 2
 
+let itemx = bx*1.2
+let itemy = by*0.5
+
 let boxx = w 
 
 let gay
@@ -16,7 +19,7 @@ function returnhome() {
 }
 
 function preload() {
-    gay = loadImage("berrybobjones.png")
+    bobjones = loadImage("berrybobjones.png")
     apple = loadImage("apple.png")
 }
 
@@ -26,7 +29,7 @@ function setup() {
 }
 
 function goal() {
-    image(apple,bx*1.2,by*0.5,100,100)
+    image(apple,itemx,itemy,100,100)
 }
 
 function figure() {
@@ -37,9 +40,12 @@ function figure() {
     //function to setup the initial player position
     this.show = function () {
         //determine what it will look like
-        image(gay,this.x,this.y,50,50)
+        image(bobjones,this.x,this.y,50,50)
     }
 }
+
+let hit = false
+let points = 0
 
 function draw() {
     clear()
@@ -48,6 +54,7 @@ function draw() {
     createCanvas(w,h)
     player.show()
     goal()
+    hit = collideRectCircle(player.x,player.y,50,50,itemx+50,itemy+50,100,100)
     let speed = 5
 
     //conditional statement, if the x of the ball goes =0 or <0, the conditional wont run
@@ -74,8 +81,18 @@ function draw() {
             player.y += speed
         }
     }
+
+    if (hit == true) {
+        document.getElementsByClassName('directions').innerText = 'press spacebar to interact'
+        print('collision!', hit);
+        if (keyIsPressed === 32) {
+            applex = -100
+            appley = -100
+            document.getElementsByClassName('directions').innerText = "tutorial complete!"
+        }
+    }
+    else if (hit == false) {
+        print('no')
+    }
 }
 
-function box() {
-    rect(boxx,boxy)
-}
